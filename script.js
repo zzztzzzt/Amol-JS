7fa46a16f9287aa13603838ed479814fc7a0ae76
zayzay <zayzayzay98765@gmail.com>
2024-11-06 22:57:54 +0800

    Optimize time control

--- script.js
+++ script.js
@@ -1,37 +1,52 @@
 import * as AMOL from './amol';
 
-let cameraStaus = "go";
-
-const myMovie = AMOL.movie('forgotten', -1, window.innerWidth, window.innerHeight, 0, 0);
+const CameraStatus = {
+    GO: 'go',
+    BACK: 'back',
+};
+let cameraStatus = CameraStatus.GO;
 
 // cameraSpeedRotate() and stopCameraRotate() can only run in movie scene currently.
+const myMovie = AMOL.movie('forgotten', -1, window.innerWidth, window.innerHeight, 0, 0);
+
+const adjustCamera = (speedX, speedY, speedZ, rotateX, rotateY, rotateZ, newStatus, delay = 3000, frameDelay = (1000 / 60)) => {
+    let lastFrameTime = 0;
+    const startTime = performance.now();
+
+    // Use requestAnimationFrame() to replaces setTimeout( ()=>{}, 3000 )
+    const animate = (time) => {
+        if (time - lastFrameTime >= frameDelay) {
+            lastFrameTime = time - (time - lastFrameTime) % frameDelay;
+
+            const elapsedTime = time - startTime;
+
+            myMovie.cameraSpeed(speedX, speedY, speedZ);
+            myMovie.cameraSpeedRotate(rotateX, rotateY, rotateZ);
+
+            if (elapsedTime >= delay) {
+                myMovie.stopCamera();
+                myMovie.stopCameraRotate();
+                cameraStatus = newStatus;
+            }
+        }
+        requestAnimationFrame(animate);
+    };
+    requestAnimationFrame(animate);
+};
+
 const myButton = myMovie.create('amol-button-golden', 0);
 myButton.setPosition(2.5, -1, -3);
-myButton.setListener('click', () => {
-    if (cameraStaus == "go") {
-        myMovie.cameraSpeed(0.04, 0, -0.06);
-        myMovie.cameraSpeedRotate(0, 0.004, 0);
-        setTimeout(() => {
-            myMovie.stopCamera();
-            myMovie.stopCameraRotate();
-            cameraStaus = "back";
-        }, 3000);
+myButton.setListener("click", () => {
+    if (cameraStatus === CameraStatus.GO) {
+        adjustCamera(0.04, 0, -0.06, 0, 0.004, 0, CameraStatus.BACK);
     }
 });
 
-// cameraSpeedRotate() and stopCameraRotate() can only run in movie scene currently.
 const myButton2 = myMovie.create('amol-button-golden', 0);
 myButton2.setPosition(-2.5, -1, -3);
-myButton2.setScale(0.8);
-myButton2.setListener('click', () => {
-    if (cameraStaus == "back") {
-        myMovie.cameraSpeed(-0.04, 0, 0.06);
-        myMovie.cameraSpeedRotate(0, -0.004, 0);
-        setTimeout(() => {
-            myMovie.stopCamera();
-            myMovie.stopCameraRotate();
-            cameraStaus = "go";
-        }, 3000);
+myButton2.setListener("click", () => {
+    if (cameraStatus === CameraStatus.BACK) {
+        adjustCamera(-0.04, 0, 0.06, 0, -0.004, 0, CameraStatus.GO);
     }
 });
 
