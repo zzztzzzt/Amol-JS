e81064df66d4df6c4e8e4c048db61ab5f3759067
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-01-24 23:51:52 +0800

    [refactor] support modular imports and remove main entry point

---
+++ src/setup.js
@@ -0,0 +1,10 @@
+import { AmolScene } from "../AMOL3D/amol-setup-beta.js";
+
+const setup = {
+  jsVer: (divId, cssDivId) => {
+    return new AmolScene(divId, cssDivId);
+  },
+  amolVue: () => {}
+};
+
+export { setup, AmolScene };
